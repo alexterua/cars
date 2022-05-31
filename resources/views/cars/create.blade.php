b820f94d764ab73cc85f6198ba427bfe6de2fbf7
alexterua <alexter.od.ua@gmail.com>
2022-05-31 19:16:56 +0300

    Added output errors in form create car

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -10,22 +10,47 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" id="title" name="title">
+                @error('title')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="desc" class="form-label">Description</label>
                 <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
+                @error('desc')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="engine" class="form-label">Engine</label>
                 <input type="text" class="form-control" id="engine" name="engine">
+                @error('engine')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="fuel" class="form-label">Fuel</label>
                 <input type="number" class="form-control" id="fuel" name="fuel">
+                @error('fuel')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
                 <input type="number" class="form-control" id="price" name="price">
+                @error('price')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
             <a href="{{ route('cars.index') }}" class="btn btn-dark">Back</a>
