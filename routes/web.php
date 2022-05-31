4eeb5e67ef9681737aa6abe50a1d471e18ee379c
alexterua <alexter.od.ua@gmail.com>
2022-05-31 17:57:11 +0300

    Added IndexController and CarController, created base view and base routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [IndexController::class, 'index'])->name('index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
